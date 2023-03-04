6f5c82658b77543f10988bb1dbecb03d10592c2d
JaskierBard <mateuszswiderski009@gmail.com>
2023-03-04 14:25:12 +0100

    created the basic look of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import {TestInterface} from 'types';
-import './App.css';
 
-
-function App() {
-
-  const foobar: TestInterface = {
-    x: 123,
-  }
+export const App = () => {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>PomodoroEq</h1>
+        <button>Click</button>
       </header>
-    </div>
+      <div className='bgc'></div>
+    </>
   );
 }
 
-export default App;
