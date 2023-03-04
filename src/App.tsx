b36e1c5e186bd09bd6658a55995be12bcd34c4cd
JaskierBard <mateuszswiderski009@gmail.com>
2023-03-04 18:04:59 +0100

    created primitive 14-days callendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 
 export const App = () => {
+  const [day, setDay] = useState<string[]>([]);
+
+
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+ 
+  
+
+  useEffect(() => {
+
+    const days = []
+      for (let i = 0; i < 14; i++) {
+        const d = new Date()    
+            d.setDate(d.getDate()+i);
+        days.push(String(d.getDate())+ (monthNames[d.getMonth()]))
+      }
+      setDay(days)
+}, []);
+
 
   return (
     <>
       <header>
         <h1>PomodoroEq</h1>
-        <button>Click</button>
+        
+
+        <div className='tomato'>0</div>
+        <div className='seed'>0</div>
+
+
+
       </header>
-      <div className='bgc'></div>
+      <div className='bgc'>
+        <ul className='Callendar'>
+          {day.map((item, index) => <li className='dayCallendar' key={index}><strong>{item}</strong></li>)}
+        </ul>
+      </div>
+      
+      
     </>
   );
 }
