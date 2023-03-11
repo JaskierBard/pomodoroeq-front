009b378f8cf8ee708f8f49e8254436bdf1fc8278
JaskierBard <mateuszswiderski009@gmail.com>
2023-03-11 21:42:31 +0100

    working pause - resume button

--- src/components/Clock/clock.tsx
+++ src/components/Clock/clock.tsx
@@ -1,28 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import {ClockInterface} from 'types'
-import Countdown, { zeroPad } from 'react-countdown';
+import Countdown, { calcTimeDelta, zeroPad } from 'react-countdown';
 
 
 export const Clock = () => {
-    const [pause, setPause] = useState<boolean>(false);
+    const [pause, setPause] = useState<boolean>(true);
+    const [dateStart, setDateStart] = useState<number>(Date.now()+ (1000  * 60 * 25));
 
+    const update = (Date.now()+ (1000  * 60 * 25)) - dateStart
     const learningTime = Date.now() + (1000  * 60 * 25)
-    const breakeTime = Date.now() + (1000  * 60 * 5)
-   
 
-    const renderer = ({ minutes, seconds, completed, api}:ClockInterface) => {
-        document.title = ((minutes+1).toString()) + ' minutes left';
-        if(pause === true){
+  
+
+    useEffect(() => {
+        setDateStart(update)
+
+
+  }, [pause]);
+
+ 
+
+    const renderer = ({completed, api}: ClockInterface) => {
+      
+
+        if (!completed) {
+          if(!pause)  {
             api.start()
-        } else {
+            // console.log(dateStart)
+            return <span>{zeroPad(calcTimeDelta(dateStart).minutes)}:{zeroPad(calcTimeDelta(dateStart).seconds)}</span>;
+
+  
+          } else {
             api.pause()
-        }
-        
-        if (completed) {
-          window.alert('Time is out!')
-          return <span>Time is out!</span>;
+            console.log(dateStart)
+
+            return <span>{zeroPad(calcTimeDelta(update).minutes)}:{zeroPad(calcTimeDelta(update).seconds)}</span>;
+
+  
+          }
         } else {
-          return <span>{zeroPad(minutes)}:{zeroPad(seconds)}</span>;
+          return <span>Time is out!</span>;
+
+        
         }
       };
 
@@ -31,12 +50,8 @@ export const Clock = () => {
     
       return <>
         <div className='clock'>
-        <Countdown date={learningTime} renderer={renderer}  autoStart={false}  controlled={false}></Countdown>
-        <button onClick={() => setPause(true)}>Start</button>
-        <button onClick={() => setPause(false)}>Pause</button>
-        <button onClick={() => setPause(false)}>Reset</button>
-
-
+          <Countdown date={learningTime} renderer={renderer}  autoStart={false} controlled={false}></Countdown>
+          <button onClick={() =>setPause(!pause)}>Click!</button>
         </div>
       </>
       
