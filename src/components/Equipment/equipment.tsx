4bd26c76963dff22976ed733207c049c04c87ae0
JaskierBard <mateuszswiderski009@gmail.com>
2023-04-03 20:48:54 +0200

    Added equipment

--- src/components/Equipment/equipment.tsx
+++ src/components/Equipment/equipment.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Equipment.css'
 import axios from 'axios';
 
@@ -7,25 +7,57 @@ interface Props {
  }
 
 export const Equipment = (props:Props) => {
+    const [data, setData] = useState<any>(null);
 
+    useEffect(() => {
+       getEq()
+      },[]);
 
 
     const getEq = async () => {
-        console.log(props.user.accessToken)
+        // console.log(props.user.userId)
         try {
-          await axios.get("http://localhost:3001/api/equipment" , 
-          { 
-            headers: { authorization: "Bearer " + props.user.accessToken },
-            data: { id: props.user.userId } }
-          );
-          console.log('usuwam dane...')
+          
+         const res = await fetch(`http://localhost:3001/api/equipment`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                userId: props.user.userId,
+            } )
+        });
+        const ol = await res.json()
+
+        setData(ol['0']);
         } catch (err) {
-        }
+        
+    }
+
       };
-   
     return (
         <>
-          <div onClick={getEq} className='equipment'></div>
+        {data ? ( 
+            <div className='eq-container'>
+            <ul>
+                <li>Pomidor: {data.tomato}</li>
+                <li>Ogórek: {data.cucumber}</li>
+                <li>Dynia: {data.pumpkin}</li>
+                <li>Nasiona pomidora: {data.tomatoSeed}</li>
+                <li>Nasiona ogórka: {data.cucumberSeed}</li>
+                <li>Nasiona dyni: {data.pumpkinSeed}</li>
+            </ul>
+            </div>
+            
+            
+
+    
+
+  ) : (
+
+    <h1>Spinner</h1>
+
+  )}
         </>
       );
 }
--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -46,7 +46,22 @@ export const LogIn = () => {
         }
       },[]);
 
-    
+      const axiosJWT = axios.create()
+          axiosJWT.interceptors.request.use(
+            async (config) => {
+              let currentDate = new Date();
+              setDecodedToken(jwt_decode(user.accessToken)) ;
+      
+              if (decodedToken.exp * 1000 < currentDate.getTime()) {
+                const data = await refreshToken();
+                config.headers["authorization"] = "Bearer " + data.accessToken;
+              }
+              return config;
+            },
+            (error) => {
+              return Promise.reject(error);
+            }
+          );
 
 
     const handleSubmit = async (e:any) => {
